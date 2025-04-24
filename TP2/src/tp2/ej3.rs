fb6804407cd0ebde53615b9928a792fb06fb297c
EzeLRA <93952326+EzeLRA@users.noreply.github.com>
2025-04-24 12:31:28 -0300

    Update ej3.rs

--- TP2/src/tp2/ej3.rs
+++ TP2/src/tp2/ej3.rs
@@ -1,5 +1,5 @@
 //Funciones Publicas
-pub fn sumar_pares(arr : &[i32]) -> i32{
+pub fn sumar_pares<const N:usize>(arr : [i32;N]) -> i32{
 	let mut res = 0;
 	for num in arr.iter(){
 		if num % 2 == 0 {res += num;}
