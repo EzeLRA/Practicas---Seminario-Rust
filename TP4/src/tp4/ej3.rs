044061a4bf30a027cc8f4205408a013e5cf9c4f2
EzeLRA <ezeleocraft@gmail.com>
2025-05-24 21:44:49 -0300

    tp4

--- TP4/src/tp4/ej3.rs
+++ TP4/src/tp4/ej3.rs
@@ -1,20 +1,14 @@
 /*
-	Estructuras secundarias : Suscripciones , Medios de pago y usuario
+	Estructuras secundarias : Suscripciones , Medios de pago y 
 */
 
-#[derive(Debug,Clone)]
-pub struct Suscripcion{
-	costo_mensual : f64,
-	duracion_mes : u8,
-	fecha_inicio : u16
-}
-#[derive(Debug,Clone)]
+#[derive(PartialEq,Debug,Clone)]
 pub enum Suscripciones{
-	Basic(Suscripcion),
-	Clasic(Suscripcion),
-	Super(Suscripcion)
+	Basic,
+	Clasic,
+	Super
 }
-#[derive(Debug,Clone)]
+#[derive(PartialEq,Debug,Clone)]
 pub enum Medios_de_pago{
 	Efectivo,
 	Mercado_pago,
@@ -22,24 +16,26 @@ pub enum Medios_de_pago{
 	Tarjeta_de_credito,
 	Criptomoneda
 }
-#[derive(Debug,Clone)]
-pub struct Usuario_datos{
-	nombre : String,
-	dni : u16
-}
 
 /*
-	Estructura primaria : Suscripcion activa
+	Estructura primaria : Usuario
 */
 
-#[derive(Debug,Clone)]
+#[derive(PartialEq,Debug,Clone)]
 pub struct Suscripcion_activa{
-	usuario : Usuario_datos,
 	tipo_suscripcion : Suscripciones,
+	costo_mensual : f64,
+	duracion_mes : u8,
+	fecha_inicio : u16,
 	tipo_pago : Medios_de_pago
 }
 
-// Suscripcion_activa implementa los traits de usuario + medios de pago + suscripcion
+#[derive(PartialEq,Debug,Clone)]
+pub struct Usuario{
+	nombre : String,
+	dni : u16 ,
+	suscripcion : Suscripcion_activa	//Integrar Option<Suscripcion_activa>
+}
 
 /*
 	Funcionalidades secundarias : Suscripciones , Medios de pago y usuario
@@ -52,9 +48,42 @@ pub trait DatosSuscripcion{
 	fn get_duracion(self)->u8;
 	fn set_fecha_inicio(&mut self,f:u16);
 	fn get_fecha_inicio(self)->u16;
+	fn set_medio(&mut self,m:&Medios_de_pago);
+    fn get_medio(&self)->Medios_de_pago;
+    fn get_tipo(&self)->Suscripciones;
+    fn set_tipo(&mut self,t:Suscripciones);
 }
-
-impl DatosSuscripcion for Suscripcion{
+pub trait DatosUsuario{
+	fn get_nombre(&self)->String;
+	fn get_dni(&self)->u16;
+	fn set_nombre(&mut self, nom : &String);
+	fn set_dni(&mut self, dni_in : u16);
+}
+impl DatosUsuario for Usuario{
+	fn get_nombre(&self)->String{
+		return self.nombre.clone();
+	}
+	fn get_dni(&self)->u16{
+		return self.dni;
+	}
+	fn set_nombre(&mut self, nom : &String){
+		self.nombre = nom.clone();
+	}
+	fn set_dni(&mut self, dni_in : u16){
+		self.dni = dni_in;
+	}
+}
+impl Usuario{
+	fn new(nom:&String,dni_in:u16,s:&Suscripcion_activa)->Usuario{
+		return Usuario{
+			nombre : nom.clone(),
+			dni : dni_in,
+			suscripcion : s.clone()	//Integrar Option<Suscripcion_activa>
+		}
+	}
+	//Implementar cambio de estado de suscripcion (pueda elevar o releevar de tipo)
+}
+impl DatosSuscripcion for Suscripcion_activa {
 	fn set_costo(&mut self,monto : f64){
 		self.costo_mensual = monto;
 	}
@@ -73,20 +102,51 @@ impl DatosSuscripcion for Suscripcion{
 	fn get_fecha_inicio(self)->u16{
 		return self.fecha_inicio;
 	}
+	fn get_medio(&self)->Medios_de_pago{
+		return self.tipo_pago.clone();
+	}
+	fn set_medio(&mut self, m : &Medios_de_pago) {
+		self.tipo_pago = m.clone();
+	}
+	 fn get_tipo(&self)->Suscripciones{
+		return self.tipo_suscripcion.clone();
+	}
+	 fn set_tipo(&mut self,t:Suscripciones){
+		self.tipo_suscripcion = t.clone();
+	}
 }
 
-impl Medios_de_pago{
-	pub fn es_igual_a(&self, t: &Medios_de_pago) -> bool {
-        match (self, t) {
-            (Medios_de_pago::Efectivo, Medios_de_pago::Efectivo) => true,
-            (Medios_de_pago::Mercado_pago, Medios_de_pago::Mercado_pago) => true,
-            (Medios_de_pago::Transferencia_bancaria, Medios_de_pago::Transferencia_bancaria) => true,
-            (Medios_de_pago::Tarjeta_de_credito, Medios_de_pago::Tarjeta_de_credito) => true,
-            (Medios_de_pago::Criptomoneda, Medios_de_pago::Criptomoneda) => true,
-            _ => false,
-        }
-    }
-    pub fn get_medio(&self)->Medios_de_pago{
-    	return self.clone()
-    }
+/*
+	Funcionalidades primarias para usuario
+*/
+
+impl Suscripcion_activa{
+	//Funciones primarias
+	fn crear_suscripcion(tipo:Suscripciones,monto:f64,duracion:u8,fecha_ini : u16,metodo_pago:Medios_de_pago)->Suscripcion_activa{
+		return Suscripcion_activa{
+			tipo_suscripcion : tipo,
+			costo_mensual : monto,
+			duracion_mes : duracion,
+			fecha_inicio : fecha_ini,
+			tipo_pago : metodo_pago
+		}
+	}
+	fn upgrade(&mut self)->bool
+	{
+		match self.get_tipo(){
+			Suscripciones::Basic =>	self.set_tipo(Suscripciones::Clasic),
+			Suscripciones::Clasic => self.set_tipo(Suscripciones::Super),
+			Suscripciones::Super => return false,
+		}
+		return true;
+	}
+	fn downgrade(&mut self)->bool
+	{
+		match self.get_tipo(){
+			Suscripciones::Basic =>	return false,
+			Suscripciones::Clasic => self.set_tipo(Suscripciones::Basic),
+			Suscripciones::Super => self.set_tipo(Suscripciones::Clasic),
+		}
+		return true;
+	}
 }
